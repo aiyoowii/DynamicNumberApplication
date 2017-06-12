18d2e3faeb92955688012e1322a9aa9be5c22db6
cegrano <zyw881122@gmail.com>
2017-06-12 19:50:35 +0800

    width modify

--- app/src/main/java/com/cegrano/android/dynamicnumber/DialNumberView.java
+++ app/src/main/java/com/cegrano/android/dynamicnumber/DialNumberView.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -22,6 +23,9 @@ public class DialNumberView extends View {
     private Bitmap mDial;
     private Bitmap mArrow;
     private float mDensity;
+    private float degree = -90;
+    private Rect mRect;
+    private RectF mRectF;
 
     public DialNumberView(Context context) {
         super(context);
@@ -39,18 +43,26 @@ public class DialNumberView extends View {
 
         mDensity = getResources().getDisplayMetrics().density;
         mPaint.setColor(Color.WHITE);
-        mPaint.setTextSize(44 * mDensity);
+//        mPaint.setTextSize(44 * mDensity);
         mPaint.setAntiAlias(true);
 
-        String testString = "1000";
+//        String testString = "1000";
 //        mPaint.setStrokeWidth(3);
-        mPaint.setTextAlign(Paint.Align.CENTER);
-        mPaint.getTextBounds(testString, 0, testString.length(), mBounds);
+//        mPaint.setTextAlign(Paint.Align.CENTER);
+//        mPaint.getTextBounds(testString, 0, testString.length(), mBounds);
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.outWidth = getMeasuredWidth();
-        options.outHeight = (int) (getMeasuredWidth() * 0.57);
+        options.outHeight = getMeasuredHeight();
+        options.inScaled = true;
         mDial = BitmapFactory.decodeResource(getResources(), R.mipmap.dial, options);
         mArrow = BitmapFactory.decodeResource(getResources(), R.mipmap.dial_arrow, new BitmapFactory.Options());
+        mRect = new Rect(0, 0, mDial.getWidth(), mDial.getHeight());
+        mRectF = new RectF(0, 0, getMeasuredWidth(), getMeasuredHeight());
+    }
+
+    public void setNumber(int number) {
+        degree = (number - 500) / 4500f * 180 - 180;
+        postInvalidate();
     }
 
     @Override
@@ -59,13 +71,15 @@ public class DialNumberView extends View {
         if (mDial == null) {
             init();
         }
-        canvas.drawBitmap(mDial, 0, 0, mPaint);
-        canvas.drawText("1000", mDial.getWidth() / 2, 83 * mDensity + mBounds.height(), mPaint);
+        canvas.drawBitmap(mDial, mRect, mRectF, mPaint);
+//        canvas.drawBitmap(mDial, 0, 0, mPaint);
+//        canvas.drawText("1000", mDial.getWidth() / 2, 83 * mDensity + mBounds.height(), mPaint);
 
-        canvas.drawBitmap(mArrow, (float) (mDial.getWidth() / 2 + mDial.getWidth() / 2 * Math.cos(Math.PI * 135 / 180)), (float) (mDial.getWidth() / 2 - mDial.getWidth() / 2 * Math.sin(Math.PI * 135 / 180)), mPaint);
-//        canvas.translate(mDial.getWidth()/2,mDial.getWidth()/2);
-//        canvas.rotate(30);
-//        canvas.drawBitmap(mArrow, 0,0,mPaint);
+//        canvas.drawBitmap(mArrow, (float) (mDial.getWidth() / 2 + mDial.getWidth() / 2 * Math.cos(Math.PI * degree)), (float) (mDial.getWidth() / 2 - mDial.getWidth() / 2 * Math.sin(Math.PI * degree)), mPaint);
+        canvas.translate((float) (getMeasuredWidth() / 2 + getMeasuredWidth() * 0.73 / 2 * Math.cos(Math.PI * degree / 180) - mArrow.getWidth() / 2),
+                (float) (getMeasuredWidth() / 2 + getMeasuredWidth() * 0.73 / 2 * Math.sin(Math.PI * degree / 180)) - mArrow.getHeight() / 2);
+        canvas.rotate(degree + 90);
+        canvas.drawBitmap(mArrow, 0, 0, mPaint);
 
     }
 }
